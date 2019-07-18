10fec3a6cab62a31187962295a12504bcd5b5953
Lalith Suresh <lsuresh@vmware.com>
2019-07-18 10:44:12 -0700

    Update copyrights

--- weave/src/main/java/com/vrg/backend/SplitIntoSingleHeadComprehensions.java
+++ weave/src/main/java/com/vrg/backend/SplitIntoSingleHeadComprehensions.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright © 2018-2019 VMware, Inc. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: BSD-2
+ */
+
 package com.vrg.backend;
 
 import com.vrg.compiler.monoid.GroupByComprehension;
