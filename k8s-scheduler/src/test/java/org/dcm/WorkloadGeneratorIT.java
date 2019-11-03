bb8567eec169f78e74c0da341e22cf7f6b5e51ec
Lalith Suresh <lsuresh@vmware.com>
2019-11-03 11:31:25 -0800

    Configure scheduler to be used when running WorkloadGeneratorIT
    Signed-off-by: Lalith Suresh <lsuresh@vmware.com>

--- k8s-scheduler/src/test/java/org/dcm/WorkloadGeneratorIT.java
+++ k8s-scheduler/src/test/java/org/dcm/WorkloadGeneratorIT.java
@@ -7,8 +7,12 @@ package org.dcm;
 
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,16 +20,35 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * To run these specific tests, pass a `schedulerName` property to maven, for example:
+ *
+ *  mvn integrate-test -DargLine="-Dk8sUrl=<hostname>:<port> -DschedulerName=dcm-scheduler"
+ */
 public class WorkloadGeneratorIT extends ITBase {
+    private static final Logger LOG = LoggerFactory.getLogger(WorkloadGeneratorIT.class);
+    private static final String SCHEDULER_NAME_PROPERTY = "schedulerName";
+    @Nullable private static String schedulerName;
+
+    @BeforeAll
+    public static void setSchedulerFromEnvironment() {
+        schedulerName = System.getProperty(SCHEDULER_NAME_PROPERTY);
+    }
 
     @Test
     public void testAffinityAntiAffinity() throws Exception {
+        assertNotNull(schedulerName);
+        LOG.info("Running testAffinityAntiAffinity with parameters: MasterUrl:{} SchedulerName:{}",
+                 fabricClient.getConfiguration().getMasterUrl(), schedulerName);
+
         // Add a new one
-        final Deployment cacheExample = launchDeploymentFromFile("cache-example.yml");
+        final Deployment cacheExample = launchDeploymentFromFile("cache-example.yml", schedulerName);
         final String cacheName = cacheExample.getMetadata().getName();
-        final Deployment webStoreExample = launchDeploymentFromFile("web-store-example.yml");
+        final Deployment webStoreExample = launchDeploymentFromFile("web-store-example.yml",
+                                                                    schedulerName);
         final String webStoreName = webStoreExample.getMetadata().getName();
 
         final int newPodsToCreate = cacheExample.getSpec().getReplicas() + webStoreExample.getSpec().getReplicas();
