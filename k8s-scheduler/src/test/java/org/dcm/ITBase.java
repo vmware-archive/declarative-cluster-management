c6812c5c144d5626de18a72df5ce2af53decf1d7
Sangeetha Abdu Jyothi <sabdujyothi@sabdujyothi-a01.vmware.com>
2019-11-12 16:38:18 -0800

    moving to the new connection setup model in ITBase

--- k8s-scheduler/src/test/java/org/dcm/ITBase.java
+++ k8s-scheduler/src/test/java/org/dcm/ITBase.java
@@ -9,14 +9,9 @@ import io.fabric8.kubernetes.api.model.Namespace;
 import io.fabric8.kubernetes.api.model.NamespaceBuilder;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
-import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientException;
 import io.fabric8.kubernetes.client.Watcher;
-import io.fabric8.kubernetes.client.ApiClient;
-import io.fabric8.kubernetes.client.Configuration;
-import io.fabric8.kubernetes.client.apis.CoreV1Api;
-import io.fabric8.kubernetes.client.util.Config;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Timeout;
@@ -24,33 +19,19 @@ import org.junit.jupiter.api.Timeout;
 import java.io.File;
 import java.net.URL;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ITBase {
-    private static final String K8S_URL_PROPERTY = "k8sUrl";
     static final String TEST_NAMESPACE = "default"; // make sure that pod-specs use this namespace
 
-    static CoreV1Api coreV1Api;
     static DefaultKubernetesClient fabricClient;
 
     @BeforeAll
     public static void setupConnectionAndCreateNamespace() {
-        final String k8sUrl = System.getProperty(K8S_URL_PROPERTY);
-        assertNotNull(k8sUrl,
-                "k8sUrl system property has not been configured. If running this integration test " +
-                        "through maven, please run: " +
-                        "\n $: mvn integration-test -DargLine=\"-Dk8sUrl=http://<hostname>:<port>\"");
-        final ApiClient client = Config.fromUrl(k8sUrl);
-        client.getHttpClient().setReadTimeout(0, TimeUnit.SECONDS); // infinite timeout
-        Configuration.setDefaultApiClient(client);
-        coreV1Api = new CoreV1Api();
-        final io.fabric8.kubernetes.client.Config config =
-                new ConfigBuilder().withMasterUrl(k8sUrl).build();
-        fabricClient = new DefaultKubernetesClient(config);
+        fabricClient = new DefaultKubernetesClient();
 
         // Create new namespace if required
         final Namespace namespace = new NamespaceBuilder().withNewMetadata().withName(TEST_NAMESPACE).endMetadata()
@@ -73,10 +54,7 @@ public class ITBase {
         }
         waitUntil((n) -> hasDrained());
     }
-
-    Deployment launchDeploymentFromFile(final String resourceName) {
-        return launchDeploymentFromFile(resourceName, "default-scheduler");
-    }
+    
 
     Deployment launchDeploymentFromFile(final String resourceName, final String schedulerName) {
         final URL url = getClass().getClassLoader().getResource(resourceName);
