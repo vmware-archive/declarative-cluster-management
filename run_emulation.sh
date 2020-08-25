d65585a62cc3099dc8fcc0844160f1b5daec61e2
Lalith Suresh <lsuresh@vmware.com>
2020-08-25 11:59:40 -0700

    Update run_emulation.sh script
    Signed-off-by: Lalith Suresh <lsuresh@vmware.com>

--- run_emulation.sh
+++ run_emulation.sh
@@ -10,7 +10,7 @@ mkdir -p $TRACE_DIR/$GIT_REV
 for numNodes in 500 5000 10000;
 do
    startTimeCutOff=200000
-   java -cp k8s-scheduler/target/k8s-scheduler-1.0-SNAPSHOT-tests.jar:k8s-scheduler/target/k8s-dcm-scheduler.jar org.dcm.EmulatedClusterTest -n $numNodes -f v2-cropped.txt -c 100 -m 200 -t 100 -s $startTimeCutOff &> /tmp/out
+   ./gradlew runBenchmark --args="-n $numNodes -f v2-cropped.txt -c 100 -m 200 -t 100 -s $startTimeCutOff" &> /tmp/out
    
    expId=`date +%s`
    mkdir -p $TRACE_DIR/$GIT_REV/$expId
