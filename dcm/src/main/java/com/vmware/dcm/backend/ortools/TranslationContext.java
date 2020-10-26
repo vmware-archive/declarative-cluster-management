8071541ddce34891014785095fcc4b2f11fbd9fe
Lalith Suresh <lsuresh@vmware.com>
2020-10-26 16:53:57 -0700

    dcm: split TranslationContext into its own file
    Signed-off-by: Lalith Suresh <lsuresh@vmware.com>

---
+++ dcm/src/main/java/com/vmware/dcm/backend/ortools/TranslationContext.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright © 2018-2020 VMware, Inc. All Rights Reserved.
+ * SPDX-License-Identifier: BSD-2
+ */
+
+package com.vmware.dcm.backend.ortools;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Objects;
+
+/**
+ * Represents context required for code generation. It maintains a stack of blocks
+ * in the IR, that is used to correctly scope variable declarations and accesses.
+ */
+class TranslationContext {
+    private final Deque<OutputIR.Block> scopeStack;
+    private final boolean isFunctionContext;
+
+    private TranslationContext(final Deque<OutputIR.Block> declarations, final boolean isFunctionContext) {
+        this.scopeStack = declarations;
+        this.isFunctionContext = isFunctionContext;
+    }
+
+    TranslationContext(final boolean isFunctionContext) {
+        this(new ArrayDeque<>(), isFunctionContext);
+    }
+
+    TranslationContext withEnterFunctionContext() {
+        final Deque<OutputIR.Block> stackCopy = new ArrayDeque<>(scopeStack);
+        return new TranslationContext(stackCopy, true);
+    }
+
+    boolean isFunctionContext() {
+        return isFunctionContext;
+    }
+
+    void enterScope(final OutputIR.Block block) {
+        scopeStack.addLast(block);
+    }
+
+    OutputIR.Block currentScope() {
+        return Objects.requireNonNull(scopeStack.getLast());
+    }
+
+    OutputIR.Block leaveScope() {
+        return scopeStack.removeLast();
+    }
+
+    OutputIR.Block getRootBlock() {
+        return scopeStack.getFirst();
+    }
+
+    String declareVariable(final String expression) {
+        for (final OutputIR.Block block: scopeStack) {
+            if (block.hasDeclaration(expression)) {
+                return block.getDeclaredName(expression);
+            }
+        }
+        return scopeStack.getLast().declare(expression);
+    }
+
+    String declareVariable(final String expression, final OutputIR.Block block) {
+        return block.declare(expression);
+    }
+
+    String getTupleVarName() {
+        return currentScope().getTupleName();
+    }
+}
