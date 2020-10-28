ebc46bef97007a7e163bd12644050e850349f829
Lalith Suresh <lsuresh@vmware.com>
2020-10-27 19:06:30 -0700

    ortools-backend: separate out static classes into their own files
    Signed-off-by: Lalith Suresh <lsuresh@vmware.com>

---
+++ dcm/src/main/java/com/vmware/dcm/backend/ortools/GroupContext.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2018-2020 VMware, Inc. All Rights Reserved.
+ * SPDX-License-Identifier: BSD-2
+ */
+
+package com.vmware.dcm.backend.ortools;
+
+import com.vmware.dcm.compiler.monoid.GroupByQualifier;
+
+// TODO: consolidate into TranslationContext
+class GroupContext {
+    private final GroupByQualifier qualifier;
+    private final String tempTableName;
+    private final String groupViewName;
+
+    GroupContext(final GroupByQualifier qualifier, final String tempTableName, final String groupViewName) {
+        this.qualifier = qualifier;
+        this.tempTableName = tempTableName;
+        this.groupViewName = groupViewName;
+    }
+
+    public GroupByQualifier getQualifier() {
+        return qualifier;
+    }
+
+    public String getTempTableName() {
+        return tempTableName;
+    }
+
+    public String getGroupViewName() {
+        return groupViewName;
+    }
+}
