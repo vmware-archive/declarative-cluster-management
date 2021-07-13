047851ea3a62004e20b0900e4d134dd6a63e32e1
Lalith Suresh <lsuresh@vmware.com>
2021-07-13 13:26:13 -0700

    postfix
    Signed-off-by: Lalith Suresh <lsuresh@vmware.com>

--- k8s-scheduler/src/test/java/com/vmware/dcm/WorkloadReplayTest.java
+++ k8s-scheduler/src/test/java/com/vmware/dcm/WorkloadReplayTest.java
@@ -11,8 +11,7 @@ public class WorkloadReplayTest {
 
     @Test
     public void runTest() throws Exception {
-        final String[] args =
-                {"-n", "500", "-f", "v1-cropped.txt", "-c", "100", "-m", "200", "-t", "100", "-s", "1000"};
+        final String[] args = {"-n", "500", "-f", "test-data.txt", "-c", "100", "-m", "200", "-t", "100", "-s", "1000"};
         EmulatedCluster.runWorkload(args);
     }
 }
