2180a0b08f450dae75dc2386ffc9d330b0261353
Lalith Suresh <lsuresh@vmware.com>
2021-08-18 15:09:30 -0700

    k8s-scheduler: rebase with master
    Signed-off-by: Lalith Suresh <lsuresh@vmware.com>

--- k8s-scheduler/src/test/java/com/vmware/dcm/SchedulerTest.java
+++ k8s-scheduler/src/test/java/com/vmware/dcm/SchedulerTest.java
@@ -94,7 +94,9 @@ import static org.jooq.impl.DSL.table;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
